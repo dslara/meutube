22820c370067b14e60b96db56494de522e628f06
Daniel Salvador Lara <dslara@hotmail.com>
2025-02-21 06:23:37 +0100

    add fe and be apps

---
+++ apps/fe-react/src/app/app.tsx
@@ -0,0 +1,19 @@
+
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> UIII </span>
+        Welcome @meutube/fe-react 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+ 
