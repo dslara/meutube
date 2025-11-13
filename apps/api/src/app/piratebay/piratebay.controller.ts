9833301348cb437b5f64afcbfcbc0ef8b074630e
Daniel Salvador <dslara@hotmail.com>
2025-11-13 18:41:13 +0100

    (feat): webtorrent test node

--- apps/api/src/app/piratebay/piratebay.controller.ts
+++ apps/api/src/app/piratebay/piratebay.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PirateBayService } from './piratebay.service';
+import WebTorrent from 'webtorrent';
 
 @Controller('torrents')
 export class PirateBayController {
+  private client = new WebTorrent();
+
   constructor(
     private readonly pirateBayService: PirateBayService,
   ) { }
@@ -14,9 +17,12 @@ export class PirateBayController {
       this.pirateBayService.filterBySeeders(result, 10)
     )[0];
 
-    return {
-      magnet: this.pirateBayService.generateMagnetLink(torrent)
-    }
+    const link = this.pirateBayService.generateMagnetLink(torrent);
+    this.client.add(link, { path: '/home/dani/Documents/Media/Downloads' }, function (torrent) {
+      torrent.on('done', function () {
+        return 'torrent download finished'
+      })
+    })
 
   }
 }
