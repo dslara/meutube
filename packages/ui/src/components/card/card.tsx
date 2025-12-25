c162d717f823ca341048a1a936fc9c7245b751bb
Daniel Salvador <dslara@hotmail.com>
2025-12-25 11:20:01 +0100

    Update card.tsx

--- packages/ui/src/components/card/card.tsx
+++ packages/ui/src/components/card/card.tsx
@@ -10,6 +10,7 @@ export const Card = ({ image, title }: CardProps) => {
       <div className="p-4">
         <h3 className="text-lg font-semibold">{title}</h3>
       </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6"></div>
     </div>
   );
 };
